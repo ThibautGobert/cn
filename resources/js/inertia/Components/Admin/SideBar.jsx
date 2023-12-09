362702139cf238e9b4586c6dd3e7842bc123ab13
Thibaut <tg@bms-engineering.lu>
2023-12-09 19:21:47 +0100

    menu mobile

--- resources/js/inertia/Components/Admin/SideBar.jsx
+++ resources/js/inertia/Components/Admin/SideBar.jsx
@@ -1,14 +1,27 @@
 import {SidebarComponent, TreeViewComponent} from '@syncfusion/ej2-react-navigations';
-import {Link, usePage} from "@inertiajs/react";
-import {forwardRef} from "react";
+import {Link, router, usePage} from "@inertiajs/react";
+import {forwardRef, useEffect, useState} from "react";
 
 const SideBar = forwardRef((props, ref)=> {
     let sidebarObj;
     const sidebarItems = usePage().props.sidebarItems
     const fields =  { dataSource: usePage().props.sidebarItems, id: 'nodeId', text: 'nodeText', child: 'nodeChild' }
+    const isMobile = ()=> {
+        let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        // Pour les appareils mobiles
+        return /android/i.test(userAgent) || /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream;
+    }
+    const [isActive, setIsActive] = useState(!isMobile());
+
+    useEffect(()=> {
+        if(isMobile()) {
+            router.on('start', (event) => {
+                setIsActive(false)
+            })
+        }
+    }, [])
 
     function onCreate() {
-        console.log(ref)
        // console.log(ref.style)
         ref.current.element.style.visibility = '';
     }
@@ -20,14 +33,15 @@ const SideBar = forwardRef((props, ref)=> {
         </div>);
     }
 
-    return <SidebarComponent id="sidebar"
-                             ref={ref}
-                             width="250px"
-                             type={'Auto'}
-                             created={onCreate}
-                             enableGestures={false}
-                             style={{ visibility: "hidden" }}
-    >
+    return <SidebarComponent
+                id="sidebar"
+                ref={ref}
+                width="250px"
+                type={'Auto'}
+                created={onCreate}
+                enableGestures={isMobile()}
+                style={{ visibility: "hidden" }}
+        >
         <TreeViewComponent fields={fields}  expandOn='Click' nodeTemplate={nodeTemplate}/>
     </SidebarComponent>
 })
--- resources/js/inertia/Components/Front/MenuCurved/Menu.jsx
+++ resources/js/inertia/Components/Front/MenuCurved/Menu.jsx
@@ -2,6 +2,7 @@ import {useEffect, useState} from "react";
 import {AnimatePresence} from "framer-motion";
 import MenuNav from "@/inertia/Components/Front/MenuCurved/MenuNav.jsx";
 import {router, usePage} from "@inertiajs/react";
+import {motion} from "framer-motion";
 
 const Menu = ()=> {
     const [isActive, setIsActive] = useState(false);
@@ -13,9 +14,14 @@ const Menu = ()=> {
 
     return (
         <>
-            <div onClick={() => {setIsActive(!isActive)}} className="front-menu">
+            <motion.div
+                onClick={() => {setIsActive(!isActive)}}
+                className={'front-menu ' + (isActive ? ' active' : '')}
+                whileHover={{ scale: 1.05 }}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            >
                 <div className={`burger ${isActive ? "active" : ""}`}></div>
-            </div>
+            </motion.div>
             <AnimatePresence mode="wait">
                 {isActive && <MenuNav />}
             </AnimatePresence>
